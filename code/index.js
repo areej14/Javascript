762c9c56aa6a6354d961437c702abe3e71bd396d
areej14 <areej.amjad14@gmail.com>
2021-03-28 22:24:20 +0500

    added

---
+++ code/index.js
@@ -0,0 +1,19 @@
+alert("Welcome");
+console.log(2+3);
+var a = 65 ;
+
+document.write(a);
+document.write("<br/>")
+var degree = prompt("your degree" , "BSCS");
+document.write("  Your entered degree is" +" "+ degree);
+document.write("<br/>")
+document.write(typeof(a));
+for(var i=1;i<=5;i++)
+{
+    document.write("<br/>")
+    for(var j=0;j<i;j++)
+    {
+     document.write("$");
+    
+    }
+}
