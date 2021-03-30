ae2c9639a6fe4cab0036d2eb04cf49717dad2367
areej14 <areej.amjad14@gmail.com>
2021-03-30 21:16:53 +0500

    added

---
+++ session 2 code/index.js
@@ -0,0 +1,23 @@
+var l = prompt ("Enter language you want to learn");
+if(l=="Javascript" || l=="html" ||l=="css")
+{
+document.write("Welcome to web community");
+}
+else{
+document.write("try again");
+}
+ document.write("<br/>")
+var array=["a", "e", "i","o","u"]
+document.write(array);
+document.write("<br/>")
+array.push("1");
+document.write(array);
+document.write("<br/>")
+array.pop("i");
+document.write(array);
+document.write("<br/>")
+array.unshift(2);
+document.write(array);
+document.write("<br/>")
+array.shift();
+document.write("<br/>")
