c445ca5ad4dbfb6553e47453a080a447627c0cfc
areej14 <areej.amjad14@gmail.com>
2021-04-04 20:59:51 +0500

    added

---
+++ session 7 tasks/Task 1/index.js
@@ -0,0 +1,7 @@
+var str1 = String(`Ideally, every programmer should
+ have knowledge of a language that's close to the system 
+(C, Go, or C++), a language that's object-oriented 
+(Java or Python), a functional programming language 
+(Scala), and a powerful scripting language (Python and
+     JavaScript).`);
+     console.log(str1);
---
+++ session 7 tasks/Task 2/index.js
@@ -0,0 +1,33 @@
+var userName = "Areej Amjad";
+var date= Date();
+var coverLetter = `${userName} Applicant
+123 Main Street
+Anytown, USA 12345
+555-212-1234
+josephq@email.com
+${date}
+
+${userName}
+Director, Human Resources
+Acme Company
+123 Business Rd.
+Business City, NY 54321
+
+Dear Ms. Smith,
+
+I am interested in the author's assistant position at Acme Company, as advertised on LinkedIn. I am currently employed as legislative director for Assemblywoman Nora Kiel, Chairperson of the NYS Assembly. I believe that the skills and experiences I have gained at this position make me an ideal candidate for the job of author’s assistant.
+
+As legislative director, I have developed strong writing and editing skills. For example, one of my main duties is to prepare Assemblywoman Nora Kiel’s personal legislation, which deals with issues related to her position as Senior Member of the NYS Assembly Standing Committee.
+
+This duty requires meticulous writing and editing skills, and an ability to convey complex legal ideas clearly. I have prepared dozens of pieces of legislation and received praise for the clarity of my writing.
+
+I have also gained extensive experience in legal and policy research – fields that you state the author’s assistant must be familiar with. My experience in the NYS Assembly has afforded me the opportunity to become familiar with the consolidated and unconsolidated laws of the State of New York. In particular, through my work with Assemblywoman Nora Kiel, I have become heavily involved in the current welfare and Medicaid reform movement. I am always eager to learn more about state legislation, reading up on these topics on my own time to become more knowledgeable. I would love to bring this passion for policy and law to your company.
+
+I am confident that my experience in the Legislature and my research and writing skills qualify me for consideration. If you would like, I can provide you with current samples of my work. I have also enclosed my resume. I look forward to meeting with you and discussing my qualifications in more detail.
+
+Sincerely,
+
+${userName} Applicant (signature hard copy letter)
+
+${userName} Applicant`;
+console.log(coverLetter);
